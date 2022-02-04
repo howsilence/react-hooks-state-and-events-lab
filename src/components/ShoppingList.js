790323d446def0f54424707f412cc247ec970235
howsilence <statton0104@gmail.com>
2022-02-03 20:34:15 -0500

    commit tree 2743952d553185c9b5338f29b760e1e5a6e471b1

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  // const [selectedCatagory, setSelectedCatagory] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  function handleCategoryChange(event){
+    // setSelectedCatagory(true)
+    setSelectedCategory(event.target.value);
+    // notice the value attributes, we grab them by event.target.value
+  }
+  // selectedCatagory.map(items =>{
+  //   items.filter(item =>{
+  //     item.selectedCatagory === selectedCatagory ? {...items }
+  //   })
+  // })
+  const itemsToDisplay = items.filter((item => {
+      // selectedCategory === "All" ? item.true : item.category === selectedCategory})
+      if (selectedCategory === "All") return true;
+
+      return item.category === selectedCategory;
+
+    // this filters the items for the ones matching our item category
+    // then we need to change the value down in className items to run this function
+  }))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +36,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
